086366d34fed713af276a78b38d4b976af7ced24
Aleksey-Smouzh <xmz@tyt.by>
2023-08-19 17:57:13 +0200

    done6

--- jS/TASKS/06-data-types/finish.js
+++ jS/TASKS/06-data-types/finish.js
@@ -12,3 +12,20 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+
+let myNumber = 10
+let myString = 'cool'
+let myNull = null
+let myUndefined 
+let myBoolean = true
+let myObject = []
+let myArray = {}
+
+console.log(typeof myNumber)
+console.log(typeof myString)
+console.log(typeof myUndefined)
+console.log(typeof myBoolean)
+console.log(typeof myObject)
+console.log(typeof myArray)
+console.log(typeof myNull)
