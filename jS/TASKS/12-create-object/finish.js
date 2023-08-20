4f105be4530e3a8dc993748f11757beea2dc7fd6
Aleksey-Smouzh <xmz@tyt.by>
2023-08-20 22:43:05 +0200

    12dane

--- jS/TASKS/12-create-object/finish.js
+++ jS/TASKS/12-create-object/finish.js
@@ -8,3 +8,7 @@
  * 2. Выведите в консоль строку
  * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
  */
+
+const arr = {name: 'Alex', surname: "Pupana", favoriteNumber:4}
+
+console.log(`my name is ${arr.name} ${arr.surname} and my favorite number is ${arr.favoriteNumber}`)
